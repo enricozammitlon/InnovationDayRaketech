da52b6a40698a8d26c94f3c4070570d82d6219c8
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-27 19:47:32 +0200

    started form

--- form_1.php
+++ form_1.php
@@ -1,2 +1,12 @@
 <?php include 'header.html' ?>
+<body>
+    <div id="content">
+        <form method="POST" action="sqlite-form-database.php" name="form1" >
+            
+            <p><label for="emp_id">Name:</label>
+            <input type="text" name="emp_id" id="emp_id" placeholder="Insert Your ID Here" required></p>
+            
+        </form>
+    </div>
+</body>
 <?php include 'footer.html' ?>
