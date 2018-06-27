5f25f9021d2f0d910832119c4ed31f4c8c071707
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-27 19:17:45 +0200

    neater

--- input.php
+++ input.php
@@ -1,8 +1,12 @@
 <?php include 'header.html' ?>
+
 <body>
+    
     <div class="content">
         <h2>Select a Form to Fill</h2>
         <p><a href="form_1.php">Form 1</a></p>
     </div>
+    
 </body>
+
 <?php include 'footer.html' ?>
