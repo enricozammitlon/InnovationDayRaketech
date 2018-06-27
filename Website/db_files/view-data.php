81bf35f0843f3fa60dcaaf5b778949dd02b2925d
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 19:12:23 +0200

    Fix for echo

--- Website/db_files/view-data.php
+++ Website/db_files/view-data.php
@@ -10,14 +10,14 @@ $con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
 //$sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
 $sql="SELECT * FROM form_data";
 $result=pg_query($con, $sql) or die('Query failed');
-echo "</p> $result[0] . '\n'</p>";
-pg_close($con);
 
-/*
+
+
     foreach (pg_fetch_row($result) as $a)
     {
-        $row=pg_fetch_array($result);
+        $row=pg_fetch_array($a);
         echo $row['emp_id'];
     }
-*/
+pg_close($con);
+
 ?>
