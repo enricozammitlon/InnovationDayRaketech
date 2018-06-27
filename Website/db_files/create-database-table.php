f0aa772e18e0acdf592762088ee031f6e497ebfe
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 18:30:03 +0200

    fix again

--- Website/db_files/create-database-table.php
+++ Website/db_files/create-database-table.php
@@ -27,7 +27,7 @@ echo $row[0] . "\n";
 
 $columns = '"id" INTEGER PRIMARY KEY NOT NULL , "emp_id" TEXT, "rate1" INTEGER, "rate2" INTEGER, "rate3" INTEGER, "rate4" INTEGER, "q1" TEXT, "q2" TEXT, "q3" TEXT , "q4" TEXT, "q5" TEXT, "q6" TEXT, "q7" TEXT, "q8" TEXT, "box1" INTEGER, "q9" TEXT';
 
-$sql = 'CREATE TABLE "form_data" (' . $columns . ')';
+$sql = 'CREATE TABLE form_data (' . $columns . ')';
 pg_query($con, $sql) or die('Create table failed');
 
 
@@ -35,7 +35,7 @@ echo '<p>Database table created with columns:</p>';
 echo "<p>$columns</p>";
 echo '<p><b>Adding example data to the database table...</b></p>';
 
-$sql="INSERT INTO 'form_data' VALUES (1,'ABC123', 1, 1, 1, 1, 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 1, 'text')";
+$sql="INSERT INTO form_data VALUES (1,'ABC123', 1, 1, 1, 1, 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 1, 'text')";
 pg_query($con, $sql) or die('Add data failed');
 
 
