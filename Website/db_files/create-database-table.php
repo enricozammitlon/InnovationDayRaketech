90abd8a690ce5e64ed86105e0c1d5e98d13f0e30
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 18:07:31 +0200

    testing postgre

--- Website/db_files/create-database-table.php
+++ Website/db_files/create-database-table.php
@@ -4,23 +4,35 @@ define('ISITSAFETORUN', TRUE);
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$databasename ='formcreator.sqlite';
-$db = new SQLite3($databasename);
+$host="ec2-54-228-251-254.eu-west-1.compute.amazonaws.com" ;
+$dbname="d59p9596i46fc";
+$user= "kpglhkjfgjqlly";
+$password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
 
-echo '<p><b>Removing any existing SQLite form database table...</b></p>';
 
-$sql = 'DROP TABLE IF EXISTS "form_data"';
-$db->exec($sql) or die('Drop table failed');
-
-echo '<p>Database table removed.</p>'; 
 echo '<p><b>Creating new SQLite database table...</b></p>';
 
+$con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
+    or die ("Could not connect to server\n");
+
+
+$query = "SELECT VERSION()";
+$rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+$row = pg_fetch_row($rs);
+
+echo $row[0] . "\n";
+
+pg_close($con);
+
+?>
+<?php
+/*
 $columns = '"id" INTEGER PRIMARY KEY NOT NULL , "emp_id" TEXT, "rate1" INTEGER, "rate2" INTEGER, "rate3" INTEGER, "rate4" INTEGER, "q1" TEXT, "q2" TEXT, "q3" TEXT , "q4" TEXT, "q5" TEXT, "q6" TEXT, "q7" TEXT, "q8" TEXT, "box1" INTEGER, "q9" TEXT';
 
 $sql = 'CREATE TABLE "form_database" (' . $columns . ')';
 $db->exec($sql) or die('Create table failed');
 
-echo '<p>Database table created with columns:</p>'; 
+echo '<p>Database table created with columns:</p>';
 echo "<p>$columns</p>";
 echo '<p><b>Adding example data to the database table...</b></p>';
 
@@ -28,7 +40,7 @@ $sql="INSERT INTO walk VALUES (1,'ABC123', 1, 1, 1, 1, 'text', 'text', 'text', '
 $db->exec($sql) or die('Add data failed');
 
 
-echo '<p>Data added to table:</p>'; 
+echo '<p>Data added to table:</p>';
 
 echo '<pre>';
 $results = $db->query("SELECT * FROM form_data");
