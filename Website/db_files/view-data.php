1b5e845a5bd23b928b0a7366ba6caa6fe11f9d9b
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 19:21:10 +0200

    testing row counter

--- Website/db_files/view-data.php
+++ Website/db_files/view-data.php
@@ -7,13 +7,22 @@ $password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
 $con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
     or die ("Could not connect to server\n");
 
+$sql="SELECT * FROM form_data";
+$result=pg_query($con, $sql) or die('Query failed');
+$numofrows=0;
+while (pg_fetch_row($result))
+{
+  $numofrows++;
+}
+
+
 //$sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
 $sql="SELECT * FROM form_data";
 $result=pg_query($con, $sql) or die('Query failed');
 
 
 $i=0;
-    while ($i<1)
+    while ($i<$numofrows)
     {
         $row=pg_fetch_array($result);
         echo $row["emp_id"];
