8bc737656f3c7539beb9c8c897c4903174a6f4f9
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 18:37:03 +0200

    Removed SQL3

--- Website/db_files/view-data.php
+++ Website/db_files/view-data.php
@@ -1,8 +1,11 @@
 <?php
 define('ISITSAFETORUN', TRUE);
-$databasename = 'formcreator.sqlite';
-$mytable = form_data;
-$db = new SQLite3($databasename);
+$host="ec2-54-228-251-254.eu-west-1.compute.amazonaws.com" ;
+$dbname="d59p9596i46fc";
+$user= "kpglhkjfgjqlly";
+$password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
+$con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
+    or die ("Could not connect to server\n");
 
 $sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
 
