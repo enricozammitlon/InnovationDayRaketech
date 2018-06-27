19611766d7d5b9c43abace0b29b778e52f6a46fe
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-27 20:18:03 +0200

    removed this

--- form_1.php
+++ form_1.php
@@ -8,16 +8,16 @@
             <input type="text" name="emp_id" id="emp_id" placeholder="Insert Your ID Here" required></p>
             
             <p><label for="rate1">Please rate your relationship with your team:</label>
-            Poor<input type="range" min="1" max="5" name="rate1" id="rate1" required></p>Excellent
+            <input type="range" min="1" max="5" name="rate1" id="rate1" required></p>
                       
             <p><label for="rate2">Please rate your relationship with your chapter lead:</label>
-            Poor<input type="range" min="1" max="5" name="rate1" id="rate2" required></p>Excellent
+            <input type="range" min="1" max="5" name="rate1" id="rate2" required></p>
             
             <p><label for="rate3">Do you have all the resources that you needed?</label>
-            Poor<input type="range" min="1" max="5" name="rate1" id="rate3" required></p>Excellent
+            <input type="range" min="1" max="5" name="rate1" id="rate3" required></p>
             
             <p><label for="rate4">How would you rate the organisation? </label>
-            Poor<input type="range" min="1" max="5" name="rate1" id="rate4" required></p>Excellent
+            <input type="range" min="1" max="5" name="rate1" id="rate4" required></p>
             
             <input type="submit" id="submit" name="submit" value="Submit Application">
         </form>
