b85b0649efae7c19127d14ab0a2c3cb040879f56
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 18:43:45 +0200

    Ammended echo

--- Website/db_files/view-data.php
+++ Website/db_files/view-data.php
@@ -12,27 +12,7 @@ $sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7,
 $result=pg_query($con, $sql) or die('Query failed');
     foreach (pg_fetch_row($result) as $row)
     {
-        echo
-        "<div class=results>
-            <p> Form ID: " . htmlspecialchars($row['id']) . "</p>" .
-            "<p>emp_id: " . htmlspecialchars($row['emp_id']) . "</p>" .
-            "<p>rate1: " .  htmlspecialchars($row['rate1']) . "</p>" .
-            "<p>rate2: " .  htmlspecialchars($row['rate2']) . "</p>" .
-            "<p>rate3: " .  htmlspecialchars($row['rate3']) . "</p>" .
-            "<p>rate4: " .  htmlspecialchars($row['rate4']) . "</p>" .
-            "<p>q1: " .  htmlspecialchars($row['q1']) . "km</p>" .
-            "<p>q2: " .  htmlspecialchars($row['q2']) . "</p>" .
-            "<p>q3: " .  htmlspecialchars($row['q3']) . "</p>" .
-            "<p>q4: " .  htmlspecialchars($row['q4']) . "</p>" .
-            "<p>q5: " .  htmlspecialchars($row['q5']) . "</p>" .
-            "<p>q6: " .  htmlspecialchars($row['q6']) . "</p>" .
-            "<p>q7: " .  htmlspecialchars($row['q7']) . "</p>" .
-            "<p>q8: " .  htmlspecialchars($row['q8']) . "</p>" .
-            "<p>box1: " .  htmlspecialchars($row['box1']) . "</p>" .
-            "<p>q9: " .  htmlspecialchars($row['q9']) . "</p>" .
-            "<p>--------------------------------------" . "</p>
-        </div>";
-        echo "</p>";
+        echo $row
         }
 
 ?>
