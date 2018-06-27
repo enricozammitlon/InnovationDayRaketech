ece59cfe5aa2cfb73d0aeb5d6916600b949803e4
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-27 20:10:47 +0200

    to check

--- form_1.php
+++ form_1.php
@@ -8,16 +8,16 @@
             <input type="text" name="emp_id" id="emp_id" placeholder="Insert Your ID Here" required></p>
             
             <p><label for="rate1">Please rate your relationship with your team:</label>
-            <input type="range" min="1" max="5" name="rate1" id="rate1" required></p>
+            Poor<input type="range" min="1" max="5" name="rate1" id="rate1" required></p>Excellent
                       
             <p><label for="rate2">Please rate your relationship with your chapter lead:</label>
-            <input type="range" min="1" max="5" name="rate1" id="rate2" required></p>
+            Poor<input type="range" min="1" max="5" name="rate1" id="rate2" required></p>Excellent
             
             <p><label for="rate3">Do you have all the resources that you needed?</label>
-            <input type="range" min="1" max="5" name="rate1" id="rate3" required></p>
+            Poor<input type="range" min="1" max="5" name="rate1" id="rate3" required></p>Excellent
             
             <p><label for="rate4">How would you rate the organisation? </label>
-            <input type="range" min="1" max="5" name="rate1" id="rate4" required></p>
+            Poor<input type="range" min="1" max="5" name="rate1" id="rate4" required></p>Excellent
             
             <input type="submit" id="submit" name="submit" value="Submit Application">
         </form>
