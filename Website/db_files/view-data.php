7b75191717fd0103a58c5d180fd873b90412be76
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-27 18:59:40 +0200

    Test

--- Website/db_files/view-data.php
+++ Website/db_files/view-data.php
@@ -7,14 +7,16 @@ $password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
 $con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
     or die ("Could not connect to server\n");
 
-$sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
-
+//$sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
+$sql="SELECT row_number() over() FROM form_data";
 $result=pg_query($con, $sql) or die('Query failed');
+echo $result[0] . "\n";
 
+/*
     foreach (pg_fetch_row($result) as $a)
     {
         $row=pg_fetch_array($result);
         echo $row['emp_id'];
     }
-
+*/
 ?>
