8e422b916ec80a5df67ee20b85f43548b1682d91
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-28 11:15:26 +0200

    form updates

--- view.php
+++ view.php
@@ -85,71 +85,71 @@ pg_close($con);
             <div class="slider-section">
 
               <label for="rate1">Please rate your relationship with your team: <span id="status1"></span></label><br>
-              <p><input type="range" min="1" max="5" name="rate1" id="rate1" required></p>
+              <p><input type="range" min="1" max="5" name="rate1" id="rate1" readonly></p>
 
               <label for="rate2">Please rate your relationship with your chapter lead: <span id="status2"></span></label><br>
-              <p><input type="range" min="1" max="5" name="rate2" id="rate2" required></p>
+              <p><input type="range" min="1" max="5" name="rate2" id="rate2" readonly></p>
 
               <label for="rate3">Do you have all the resources that you needed? <span id="status3"></span></label><br>
-              <p><input type="range" min="1" max="5" name="rate3" id="rate3" required></p>
+              <p><input type="range" min="1" max="5" name="rate3" id="rate3" readonly></p>
 
               <label for="rate4">How would you rate the organisation? <span id="status4"></span></label><br>
-              <p><input type="range" min="1" max="5" name="rate4" id="rate4" required></p>
+              <p><input type="range" min="1" max="5" name="rate4" id="rate4" readonly></p>
             </div>
 
 
             <div class="question-section">
               <div class="wrap-input3 validate-input">
                 <label for="q1">What is it about your job that makes you want to jump out of bed in the morning?</label><br>
-                <p><textarea class="input3" id="q1" cols="70" rows="10" name="q1"></textarea></p>
+                <p><textarea class="input3" id="q1" cols="70" rows="10" name="q1" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q2">What makes it a great day at work?</label><br>
-                <p><textarea class="input3" id="q2" cols="70" rows="10" name="q2"></textarea></p>
+                <p><textarea class="input3" id="q2" cols="70" rows="10" name="q2" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q3">Do you feel that your work makes a difference in the company?</label><br>
-                <p><textarea class="input3" id="q3" cols="70" rows="10" name="q3"></textarea></p>
+                <p><textarea class="input3" id="q3" cols="70" rows="10" name="q3" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q4">Do you feel that your coworkers think that you make a difference in the company?</label><br>
-                <p><textarea class="input3" id="q4" cols="70" rows="10" name="q4"></textarea></p>
+                <p><textarea class="input3" id="q4" cols="70" rows="10" name="q4" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q5">Do you feel “fully utilized” in your current role?</label><br>
-                <p><textarea class="input3" id="q5" cols="70" rows="10" name="q5"></textarea></p>
+                <p><textarea class="input3" id="q5" cols="70" rows="10" name="q5" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q6">Are there additional things that we can do to more fully utilize your talents and interests?</label><br>
-                <p><textarea class="input3" id="q6" cols="70" rows="10" name="q6"></textarea></p>
+                <p><textarea class="input3" id="q6" cols="70" rows="10" name="q6" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q7">If you were the chapter lead, what would you differently?</label><br>
-                <p><textarea class="input3" id="q7" cols="70" rows="10" name="q7"></textarea></p>
+                <p><textarea class="input3" id="q7" cols="70" rows="10" name="q7" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q8">Where would you like to be in the organization two years from now?</label><br>
-                <p><textarea class="input3" id="q8" cols="70" rows="10" name="q8"></textarea></p>
+                <p><textarea class="input3" id="q8" cols="70" rows="10" name="q8" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
 
               <div class="wrap-input3 validate-input">
                 <label for="q9">Would you like to add anything else? </label><br>
-                <p><textarea class="input3" id="q9" cols="70" rows="10" name="q9"></textarea></p>
+                <p><textarea class="input3" id="q9" cols="70" rows="10" name="q9" readonly></textarea></p>
                 <span class="focus-input3"></span>
               </div>
             </div>
@@ -158,14 +158,7 @@ pg_close($con);
   						<div class="contact3-form-radio m-r-42">
                 <div class="radio-section">
                   <label for="box1">Would you recommend Raketech to your family and friends?</label><br>
-                  <input class="input-radio3" id="radio1" type="radio" name="box1" value="yes" checked="checked">
-                    <label class="label-radio3" for="radio1">
-								      Yes
-							      </label>
-                  <input class="input-radio3" id="radio2" type="radio" name="box1" value="no">
-                  <label class="label-radio3" for="radio2">
-								    No
-							    </label>
+                  <p><input class="input3" type="text" name="emp_id" readonly></p>
                 </div>
               </div>
             </div>
