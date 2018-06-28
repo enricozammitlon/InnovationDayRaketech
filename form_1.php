84bb71058878851241437c4b434a92e8a91cfc50
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-28 03:47:26 +0200

    New design changes

--- form_1.php
+++ form_1.php
@@ -1,7 +1,13 @@
 <?php include 'header.html' ?>
 <body>
-    <div class="content">
-        <h2>Stay Interview</h2>
+  <div class="bg-contact3" style="background-image: url('images/bg-01.jpg');">
+		<div class="container-contact3">
+			<div class="wrap-contact3">
+				<form class="contact3-form validate-form">
+					<span class="contact3-form-title">
+            Stay Interview
+          </span>
+
         <form method="POST" action="sqlite-form-database.php" name="form1" >
 
             <input type="text" name="emp_id" id="emp_id" placeholder="Insert Your ID Here" required></p>
@@ -19,33 +25,53 @@
               <label for="rate4">How would you rate the organisation? </label><br>
               <p><input type="range" min="1" max="5" name="rate1" id="rate4" required></p>
             </div>
+
+
             <div class="question-section">
-              <label for="q1">What is it about your job that makes you want to jump out of bed in the morning?</label><br>
-              <p><textarea id="q1" cols="70" rows="10" name="q1"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q1">What is it about your job that makes you want to jump out of bed in the morning?</label><br>
+                <p><textarea id="q1" cols="70" rows="10" name="q1"></textarea></p>
+              </div>
 
-              <label for="q2">What makes it a great day at work?</label><br>
-              <p><textarea id="q2" cols="70" rows="10" name="q2"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q2">What makes it a great day at work?</label><br>
+                <p><textarea id="q2" cols="70" rows="10" name="q2"></textarea></p>
+              </div>
 
-              <label for="q3">Do you feel that your work makes a difference in the company?</label><br>
-              <p><textarea id="q3" cols="70" rows="10" name="q3"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q3">Do you feel that your work makes a difference in the company?</label><br>
+                <p><textarea id="q3" cols="70" rows="10" name="q3"></textarea></p>
+              </div>
 
-              <label for="q4">Do you feel that your coworkers think that you make a difference in the company?</label><br>
-              <p><textarea id="q4" cols="70" rows="10" name="q4"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q4">Do you feel that your coworkers think that you make a difference in the company?</label><br>
+                <p><textarea id="q4" cols="70" rows="10" name="q4"></textarea></p>
+              </div>
 
-              <label for="q5">Do you feel “fully utilized” in your current role?</label><br>
-              <p><textarea id="q5" cols="70" rows="10" name="q5"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q5">Do you feel “fully utilized” in your current role?</label><br>
+                <p><textarea id="q5" cols="70" rows="10" name="q5"></textarea></p>
+              </div>
 
-              <label for="q6">Are there additional things that we can do to more fully utilize your talents and interests?</label><br>
-              <p><textarea id="q6" cols="70" rows="10" name="q6"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q6">Are there additional things that we can do to more fully utilize your talents and interests?</label><br>
+                <p><textarea id="q6" cols="70" rows="10" name="q6"></textarea></p>
+              </div>
 
-              <label for="q7">If you were the chapter lead, what would you differently?</label><br>
-              <p><textarea id="q7" cols="70" rows="10" name="q7"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q7">If you were the chapter lead, what would you differently?</label><br>
+                <p><textarea id="q7" cols="70" rows="10" name="q7"></textarea></p>
+              </div>
 
-              <label for="q8">Where would you like to be in the organization two years from now?</label><br>
-              <p><textarea id="q8" cols="70" rows="10" name="q8"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q8">Where would you like to be in the organization two years from now?</label><br>
+                <p><textarea id="q8" cols="70" rows="10" name="q8"></textarea></p>
+              </div>
 
-              <label for="q9">Would you like to add anything else? </label><br>
-              <p><textarea id="q9" cols="70" rows="10" name="q9"></textarea></p>
+              <div class="wrap-input3 validate-input">
+                <label for="q9">Would you like to add anything else? </label><br>
+                <p><textarea id="q9" cols="70" rows="10" name="q9"></textarea></p>
+              </div>
 
             </div>
             <div class="radio-section">
@@ -54,7 +80,10 @@
               <input type="radio" name="box1" value=2>No
             </div>
 
-            <input type="submit" id="submit" name="submit" value="Submit Application">
+				   <div class="container-contact3-form-btn">
+             <input type="submit" id="submit" name="submit" value="SUBMIT">
+           </div>
+
         </form>
     </div>
 </body>
