39d5444acaf47831693bfec1436702620240403e
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-28 03:02:31 +0200

    Some design changes and table testing

--- Website/db_files/create-database-table.php
+++ Website/db_files/create-database-table.php
@@ -4,12 +4,7 @@ define('ISITSAFETORUN', TRUE);
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host="ec2-54-228-251-254.eu-west-1.compute.amazonaws.com" ;
-$dbname="d59p9596i46fc";
-$user= "kpglhkjfgjqlly";
-$password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
-$con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
-    or die ("Could not connect to server\n");
+include 'database.php';
 
 
 $query = "DROP TABLE IF EXISTS form_data";
