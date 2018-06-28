d54f41adc23e6cb0baa566e3a1c52528a2b45388
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-28 08:33:27 +0200

    test

--- form_1.php
+++ form_1.php
@@ -8,7 +8,7 @@
             Stay Interview
           </span>
 
-        <form method="post" action="/sqlite-form-database.php" name="form1" >
+        <form method="post" action="https://formcreatorhr.herokuapp.com/Website/db_files/sqlite-form-database.php" name="form1" >
           <div class="wrap-input3 validate-input">
             <input class="input3" type="text" name="emp_id" id="emp_id" placeholder="Insert Your ID Here"></p>
           </div>
@@ -106,7 +106,7 @@
 				   <div class="container-contact3-form-btn">
              <input type="submit">
            </div>
-           <?php include "Website/db_files/sqlite-form-database.php"; ?>
+           <?php //include "Website/db_files/sqlite-form-database.php"; ?>
         </form>
     </div>
 </body>
