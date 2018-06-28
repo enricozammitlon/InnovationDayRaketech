046066150c64c2d14cfad8e442521a123b1aca83
DanSchwerner <dukeofsurvival@gmail.com>
2018-06-28 12:35:35 +0200

    ...

--- view.php
+++ view.php
@@ -11,7 +11,7 @@ $con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
 //$sql="SELECT id, emp_id, rate1, rate2, rate3, rate4, q1, q2, q3, q4, q5, q6, q7, q8, box1, q9 FROM form_data";
 $sql="SELECT * FROM form_data";
 $result=pg_query($con, $sql) or die('Query failed');
-$value1 = 4
+$value1 = 4;
 $value2 = 2;
 $value3 = 1;
 $value4 = 3;
