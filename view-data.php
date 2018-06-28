213356dad41eb6771293e811be82fc5515118834
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-28 13:40:26 +0200

    test

--- view-data.php
+++ view-data.php
@@ -51,13 +51,13 @@ $row=pg_fetch_array($result);
           <label for="rate1">Please rate your relationship with your team: <span id="status1"></span> <?php echo $row['rate1'] ?> </label><br>
           <p><input type="range" min="1" max="5" name="rate1" id="rate1" readonly value=<?php echo $row['rate1'] ?> ></p>
 
-          <label for="rate2">Please rate your relationship with your chapter lead: <?php $row['rate2'] ?><span id="status2"></span></label><br>
+          <label for="rate2">Please rate your relationship with your chapter lead:<span id="status2"></span><?php echo $row['rate2'] ?></label><br>
           <p><input type="range" min="1" max="5" name="rate2" id="rate2" readonly value=<?php echo $row['rate2'] ?>></p>
 
-          <label for="rate3">Do you have all the resources that you needed? <span id="status3"></span></label><br>
+          <label for="rate3">Do you have all the resources that you needed? <span id="status3"></span><?php echo $row['rate3'] ?></label><br>
           <p><input type="range" min="1" max="5" name="rate3" id="rate3" readonly value=<?php echo $row['rate3'] ?>></p>
 
-          <label for="rate4">How would you rate the organisation? <span id="status4"></span></label><br>
+          <label for="rate4">How would you rate the organisation? <span id="status4"></span><?php echo $row['rate4'] ?></label><br>
           <p><input type="range" min="1" max="5" name="rate4" id="rate4" readonly value=<?php echo $row['rate4'] ?>></p>
         </div>
 
@@ -65,55 +65,55 @@ $row=pg_fetch_array($result);
         <div class="question-section">
           <div class="wrap-input3 validate-input">
             <label for="q1">What is it about your job that makes you want to jump out of bed in the morning?</label><br>
-            <p><textarea class="input3" id="q1" cols="70" rows="10" name="q1" readonly value=<?php echo $row['q1'] ?>></textarea></p>
+            <p><textarea class="input3" id="q1" cols="70" rows="10" name="q1" readonly><?php echo $row['q1'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q2">What makes it a great day at work?</label><br>
-            <p><textarea class="input3" id="q2" cols="70" rows="10" name="q2" readonly value=<?php echo $row['q2'] ?>></textarea></p>
+            <p><textarea class="input3" id="q2" cols="70" rows="10" name="q2" readonly ><?php echo $row['q2'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q3">Do you feel that your work makes a difference in the company?</label><br>
-            <p><textarea class="input3" id="q3" cols="70" rows="10" name="q3" readonly value=<?php echo $row['q3'] ?>></textarea></p>
+            <p><textarea class="input3" id="q3" cols="70" rows="10" name="q3" readonly ><?php echo $row['q3'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q4">Do you feel that your coworkers think that you make a difference in the company?</label><br>
-            <p><textarea class="input3" id="q4" cols="70" rows="10" name="q4" readonly value=<?php echo $row['q4'] ?> ></textarea></p>
+            <p><textarea class="input3" id="q4" cols="70" rows="10" name="q4" readonly ><?php echo $row['q4'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q5">Do you feel “fully utilized” in your current role?</label><br>
-            <p><textarea class="input3" id="q5" cols="70" rows="10" name="q5" readonly value=<?php echo $row['q5'] ?>></textarea></p>
+            <p><textarea class="input3" id="q5" cols="70" rows="10" name="q5" readonly><?php echo $row['q5'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q6">Are there additional things that we can do to more fully utilize your talents and interests?</label><br>
-            <p><textarea class="input3" id="q6" cols="70" rows="10" name="q6" readonly value=<?php echo $row['q6'] ?> ></textarea></p>
+            <p><textarea class="input3" id="q6" cols="70" rows="10" name="q6" readonly ><?php echo $row['q6'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q7">If you were the chapter lead, what would you differently?</label><br>
-            <p><textarea class="input3" id="q7" cols="70" rows="10" name="q7" readonly value=<?php echo $row['q7'] ?>></textarea></p>
+            <p><textarea class="input3" id="q7" cols="70" rows="10" name="q7" readonly ><?php echo $row['q7'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q8">Where would you like to be in the organization two years from now?</label><br>
-            <p><textarea class="input3" id="q8" cols="70" rows="10" name="q8" readonly value=<?php echo $row['q8'] ?>></textarea></p>
+            <p><textarea class="input3" id="q8" cols="70" rows="10" name="q8" readonly ><?php echo $row['q8'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
 
           <div class="wrap-input3 validate-input">
             <label for="q9">Would you like to add anything else? </label><br>
-            <p><textarea class="input3" id="q9" cols="70" rows="10" name="q9" readonly value=<?php echo $row['q9'] ?>></textarea></p>
+            <p><textarea class="input3" id="q9" cols="70" rows="10" name="q9" readonly ><?php echo $row['q9'] ?></textarea></p>
             <span class="focus-input3"></span>
           </div>
         </div>
