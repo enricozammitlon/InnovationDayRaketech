9cb669817fb57b070c2c989fe652480570e38bf5
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2018-06-28 07:45:15 +0200

    test

--- Website/db_files/sqlite-form-database.php
+++ Website/db_files/sqlite-form-database.php
@@ -2,7 +2,12 @@
 
 define('ISITDAFETORUN', TRUE);
 $mytable = form_data;
-include 'database.php';
+$host="ec2-54-228-251-254.eu-west-1.compute.amazonaws.com" ;
+$dbname="d59p9596i46fc";
+$user= "kpglhkjfgjqlly";
+$password= "512690195e545eb8afc2567aa9992944220bacf1222154d7ff2d386c6421c300";
+$con = pg_connect("host=$host dbname=$dbname user=$user password=$password")
+    or die ("Could not connect to server\n");
 echo"<p>Connected to database</p>";
 
 ini_set('display_errors', 1);
